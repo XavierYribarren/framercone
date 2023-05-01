191c2c6222b15ce129bd201104439ac285e16558
Xaboule <xaboule@gmail.com>
2023-05-02 00:57:23 +0200

    animated

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -16,19 +16,19 @@ const coneOrange = '#ff8a00'
     <div className='fullcanvas'>
 <Canvas
 shadows={true}
-camera={{fov : 50, position:[0,10,10]}}
+camera={{fov : 50, position:[0,8,12]}}
 >
 
-    <directionalLight position={[5, 5, 6]} lookAt={[0,2,0]} intensity={2} castShadow/>
+    <directionalLight position={[5, 5, 6]} lookAt={[0,3,0]} intensity={2} castShadow/>
 <ambientLight intensity={.2}/>
-<OrbitControls position0={[0, 12, 3]}/>
+<OrbitControls position0={[0, 8, 3]}/>
 <group position={[0, 2.9, 0]} scale={0.1}>
-<Cone size={[2,4, 10, 25]} position={[0, 10, 0, 32, 1, true]} coneCol={coneOrange} />
-<Cone size={[4,5, 5, 25]} position={[0, 3, 0]}coneCol={'#ffffff'}/>
-<Cone size={[5,7, 10, 25]} position={[0, -4, 0]}coneCol={coneOrange}/>
-<Cone size={[7,8, 5, 25]} position={[0, -11, 0]}coneCol={'#ffffff'}/>
-<Cone size={[8,11, 15, 25]} position={[0, -21, 0]}coneCol={coneOrange}/>
-   <Base position={[0,-28,0]} coneCol={coneOrange} /> 
+<Cone size={[2,4, 10, 25]} position={[0, 10, 0, 32, 1, true]} coneCol={coneOrange} offsetx={20} offsety={20} offsetz={20}/>
+<Cone size={[4,5, 5, 25]} position={[0, 3, 0]}coneCol={'#ffffff'} offsetx={30} offsety={30} offsetz={30}/>
+<Cone size={[5,7, 10, 25]} position={[0, -4, 0]}coneCol={coneOrange} offsetx={30} offsety={35} offsetz={-35}/>
+<Cone size={[7,8, 5, 25]} position={[0, -11, 0]}coneCol={'#ffffff'} offsetx={-18} offsety={22} offsetz={24}/>
+<Cone size={[8,11, 15, 25]} position={[0, -21, 0]}coneCol={coneOrange} offsetx={28} offsety={13} offsetz={33}/>
+   <Base position={[0,-30,0]} coneCol={coneOrange} /> 
    </group>   
 <fog attach='fog' args={['#fbb9e8', 20, 70]} />  
 
@@ -45,7 +45,7 @@ camera={{fov : 50, position:[0,10,10]}}
 export function Ground(){
 
   return(
-    <mesh rotation={[-Math.PI/2, 0, 0]} position={[0,0,0]} receiveShadow>
+    <mesh rotation={[-Math.PI/2, 0, 0]} position={[0,-0.2,0]} receiveShadow>
       <circleGeometry args={[100,100]}  />
       <meshPhongMaterial color={'#f98ade'}/>
     </mesh>
